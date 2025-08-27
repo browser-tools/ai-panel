81b92341270cce93c0ce8f1abfe5589528f4ef61
vitalets <noginsk@rambler.ru>
2025-08-27 17:55:01 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/icon4-16.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/images/icon4-48.png" />
+        <link rel="icon" type="image/png" sizes="128x128" href="/images/icon4-128.png" />
+        <link rel="icon" type="image/svg+xml" href="/images/icon4.svg" />
+      </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
       </body>
