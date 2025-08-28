734848cc7f3313ae8bc7e1b7f1c83efcb3264883
vitalets <noginsk@rambler.ru>
2025-08-28 10:35:47 +0200

    config ts

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from 'next'
+
+// basePath: https://github.com/vercel/next.js/discussions/34173#discussioncomment-5833611
+
+const nextConfig: NextConfig= {
+  output: 'export',
+  basePath: process.env.GH_PAGES_DEPLOY ? '/ai-panel' : '',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true, // Optional: Use this if you're hosting images statically
+  },
+}
+
+export default nextConfig
